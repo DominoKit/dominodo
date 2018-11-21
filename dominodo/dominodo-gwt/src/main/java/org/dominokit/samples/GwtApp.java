0736563eb755dfad5de26fecc4766664240d4e75
Ahmad Bawaneh <akabme@gmail.com>
2018-11-21 21:52:16 +0200

    run as gwt2 and j2cl app

---
+++ dominodo/dominodo-gwt/src/main/java/org/dominokit/samples/GwtApp.java
@@ -0,0 +1,10 @@
+package org.dominokit.samples;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class GwtApp implements EntryPoint {
+
+    public void onModuleLoad() {
+        new DominoDo().run();
+    }
+}
