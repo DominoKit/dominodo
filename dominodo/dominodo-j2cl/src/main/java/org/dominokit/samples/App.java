16e9fec18f02002505689f7abdf9a22797820671
Ahmad Bawaneh <akabme@gmail.com>
2018-11-27 17:31:40 +0200

    add base-annotation

--- dominodo/dominodo-j2cl/src/main/java/org/dominokit/samples/App.java
+++ dominodo/dominodo-j2cl/src/main/java/org/dominokit/samples/App.java
@@ -2,16 +2,9 @@ package org.dominokit.samples;
 
 import com.google.gwt.core.client.EntryPoint;
 
-import java.util.Map;
-import java.util.TreeMap;
-
 public class App implements EntryPoint {
 
-    private Map<String, String> map = new TreeMap<>();
     public void onModuleLoad() {
-
-        map.put("x", "y");
-        map.values().remove("x");
         new DominoDo().run();
     }
 }
